8f650e4a83caa1e5c5855f8c61a80d056ab59fbf
Prit-Patel08 <pritpatel102008@gmail.com>
2026-07-29 23:59:32 +0530

    Add selection sort implementation in C
    Implement selection sort algorithm to sort an array.

---
+++ selection_sort.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    int a[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    for (int i = 0; i < n - 1; i++) {
+        int minIdx = i;
+        for (int j = i + 1; j < n; j++) {
+            if (a[minIdx] > a[j]) {
+                minIdx = j;
+            }
+        }
+        int temp = a[i];
+        a[i] = a[minIdx];
+        a[minIdx] = temp;
+    }
+
+    for (int i = 0; i < n; i++) {
+        printf("%d ", a[i]);
+    }
+}
