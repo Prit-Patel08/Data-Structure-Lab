792b60654f7ca7a89a87c8a4a9f9afa38e1edd2c
Prit-Patel08 <pritpatel102008@gmail.com>
2026-08-06 12:11:09 +0530

    Update p2_3.c

--- p2_3.c
+++ p2_3.c
@@ -1,67 +1,46 @@
 #include <stdio.h>
 
-struct Employee
-{
+struct employee {
+    char name[15];
     int empID;
-    char empName[50];
-    float salary;
+    float salary; 
 };
 
-struct Organization
-{
+struct organization {
+    char orgName[15];
     int orgID;
-    char orgName[50];
-    struct Employee emp[2];
+    struct employee emp[2];
 };
 
 int main()
 {
-    struct Organization org[2];
-    int i, j;
-
-    // Input
-    for(i = 0; i < 2; i++)
-    {
-        printf("\nEnter details of Organization %d\n", i + 1);
-
-        printf("Organization ID: ");
+    struct organization org[2];
+    for (int i = 0; i < 2; i++) {
+        printf("For Organization %d: ", i + 1);
+        printf("\nEnter Organization Name: ");
+        scanf("%s", &org[i].orgName);
+        printf("\nEnter Organization ID: ");
         scanf("%d", &org[i].orgID);
 
-        printf("Organization Name: ");
-        scanf(" %[^\n]", org[i].orgName);
-
-        for(j = 0; j < 2; j++)
-        {
-            printf("\nEnter details of Employee %d\n", j + 1);
-
-            printf("Employee ID: ");
-            scanf("%d", &org[i].emp[j].empID);
-
-            printf("Employee Name: ");
-            scanf(" %[^\n]", org[i].emp[j].empName);
-
-            printf("Salary: ");
+        for (int j = 0; j < 2; j++) {
+            printf("\nEnter employee %d name: ", j + 1);
+            scanf("%s", &org[i].emp[j].name);
+            printf("\nEnter employee %d ID: ", j + 1);
+            scanf("%s", &org[i].emp[j].empID);
+            printf("\nEnter employee %d salary: ", j + 1);
             scanf("%f", &org[i].emp[j].salary);
         }
     }
 
-    // Output
-    printf("\n========== Organization Details ==========\n");
+    for (int i = 0; i < 2; i++) {
+        printf("\nFor Organization %d: ", i + 1);
+        printf("\nName: %s", org[i].orgName);
+        printf("\nOrganization ID: %d", org[i].orgID);
 
-    for(i = 0; i < 2; i++)
-    {
-        printf("\nOrganization %d\n", i + 1);
-        printf("Organization ID   : %d\n", org[i].orgID);
-        printf("Organization Name : %s\n", org[i].orgName);
-
-        for(j = 0; j < 2; j++)
-        {
-            printf("\nEmployee %d\n", j + 1);
-            printf("Employee ID   : %d\n", org[i].emp[j].empID);
-            printf("Employee Name : %s\n", org[i].emp[j].empName);
-            printf("Salary        : %.2f\n", org[i].emp[j].salary);
+        for (int j = 0; j < 2; j++) {
+            printf("\nEmployee %d Name: %s", j + 1, org[i].emp[j].name);
+            printf("\nEmployee %d ID: %d", j + 1, org[i].emp[j].empID);
+            printf("\nEmployee %d salary: %f", j + 1, org[i].emp[j].salary);
         }
     }
-
-    return 0;
 }
